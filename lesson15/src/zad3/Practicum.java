d82bf5a0ae9bf4662d2a6daeb16b9a04d8c2c6aa
Denis <133685320+Y0shhir0@users.noreply.github.com>
2024-12-05 14:06:58 +0300

    Add files via upload

---
+++ lesson15/src/zad3/Practicum.java
@@ -0,0 +1,32 @@
+package zad3;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Practicum {
+    public static void main(String[] args) {
+        Set<String> songs = new LinkedHashSet<>(); // Используем LinkedHashSet для сохранения порядка
+        fillSongs(songs);
+
+        System.out.println("Количество песен: " + songs.size());
+
+        System.out.println("Песни:");
+        for (String song : songs) {
+            System.out.println("  * " + song);
+        }
+    }
+
+    private static void fillSongs(Set<String> songs) {
+        songs.add("Sting – Shape Of My Heart");
+        songs.add("Gorillaz – Clint Eastwood");
+        songs.add("Lady Gaga – Bad Romance");
+        songs.add("Taylor Swift – Wildest Dreams");
+        songs.add("Ariana Grande – 7 rings");
+        songs.add("Depeche Mode – Personal Jesus");
+        songs.add("Gorillaz – Clint Eastwood"); // Дубликат
+        songs.add("Lady Gaga – Bad Romance");   // Дубликат
+        songs.add("Bruno Mars – Talking To The Moon");
+        songs.add("Taylor Swift – Wildest Dreams"); // Дубликат
+    }
+}
+
